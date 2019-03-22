ea22f45bed21c3cb78799e5288e5290d28587949
Etienne <etienne.crespi@laposte.net>
2019-03-22 18:24:25 +0100

    Follow the tuto

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 import {inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {AuthService} from "./common/services/auth-service";
 import {PostService} from "./common/services/post-service";
 
-@inject(PostService)
+@inject(EventAggregator, AuthService, PostService)
 export class App {
 
-  constructor(PostService) {
+  constructor(EventAggregator, AuthService, PostService) {
+    this.ea = EventAggregator;
+    this.authService = AuthService;
     this.postService = PostService;
   }
 
   attached(){
+
+    this.currentUser = this.authService.currentUser;
+    // We need to click the SUBSCRIBE button to listen for data that will be published in future.
+    this.subscription = this.ea.subscribe('myEventName', user => {
+      this.currentUser = this.authService.currentUser;
+    });
 
     this.postService.allTags().then(data => {
       this.tags = data.tags;
@@ -21,16 +31,23 @@ export class App {
     }).catch(error => {
       this.error = error.message;
     });
+
   }
 
   configureRouter(config, router) {
     config.title = 'Lord\'s Blog';
     config.map([
       {route: '', name: 'home', moduleId: PLATFORM.moduleName('posts/index'), title: 'All posts'},
+      {route: 'login', name: 'login', moduleId: PLATFORM.moduleName('auth/login'), title: 'Log In'},
       {route: 'post/:slug', name: 'post-view', moduleId: PLATFORM.moduleName('posts/view'), title: 'A post'},
       {route: 'tag/:tag', name: 'tag-view', moduleId: PLATFORM.moduleName('posts/tag-view'), title: 'View Posts by Tags'},
       {route: 'archive/:archive', name: 'archive-view', moduleId: PLATFORM.moduleName('posts/archive-view'), title: 'View Posts by Archive'}
     ]);
-    this.router = router;
   }
+
+  detached(){
+    // We can also detach our subscriber by clicking the DISPOSE button.
+  this.subscription.dispose();
+  }
+
 }
---
+++ src/auth/login.js
@@ -0,0 +1,29 @@
+import {inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {Router} from 'aurelia-router';
+import {AuthService} from "../common/services/auth-service";
+
+@inject(EventAggregator, Router, AuthService)
+export class Login {
+
+  constructor(EventAggregator, Router, AuthService) {
+    this.ea = EventAggregator;
+    this.router = Router;
+    this.authService = AuthService;
+  }
+
+  activate(){
+    this.error = null;
+  }
+
+  login(){
+    this.error = null;
+    this.authService.login(this.name).then(data => {
+      this.ea.publish('myEventName', data.name);
+      this.router.navigateToRoute('home');
+    }).catch(error => {
+      this.error = error.message;
+    });
+  }
+
+}
